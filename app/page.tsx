9a0e0a190bfeeb2d3bb2c035f0cf3f94dd638cd1
EllmanT <tmuranda1@gmail.com>
2024-12-29 12:05:34 +0200

    ES Lint and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-
 export default function Home() {
   return (
-   <>
-   <h1 className="text-3xl ">Welcome to DevOverflow</h1>
-   </>
+    <>
+      <h1 className="text-3xl font-black text-white">
+        Welcome to DevOverflow the app that is super dope
+      </h1>
+    </>
   );
 }
