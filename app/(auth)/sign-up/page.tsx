df90393aeb6264d3544b11ad7913a30bc31d8f70
EllmanT <tmuranda1@gmail.com>
2025-01-02 14:17:46 +0200

    Setup Auth Layouts, Social Auth Form, Next Auth OAuth with Google and Github Providers

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div> THe sign up page Page</div>;
+};
+
+export default SignUp;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,26 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/route";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <h1 className="h2-bold font-space-grotesk">
+        Welcome to DevOverflow the app that is super dope
+      </h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
+};
+export default Home;
