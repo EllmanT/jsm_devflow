b3ee8986fd09c9b9f97624afb959b6e65b904312
EllmanT <tmuranda1@gmail.com>
2025-01-07 19:25:30 +0200

    Created the colections model

--- database/vote.model.ts
+++ database/vote.model.ts
@@ -7,12 +7,15 @@ export interface IVote {
   voteType: "upvote" | "downvote";
 }
 
-const VoteSchema = new Schema<IVote>({
-  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  id: { type: Schema.Types.ObjectId, required: true },
-  type: { type: String, enum: ["question", "answer"], required: true },
-  voteType: { type: String, enum: ["upvote", "downvote"], required: true },
-});
+const VoteSchema = new Schema<IVote>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    id: { type: Schema.Types.ObjectId, required: true },
+    type: { type: String, enum: ["question", "answer"], required: true },
+    voteType: { type: String, enum: ["upvote", "downvote"], required: true },
+  },
+  { timestamps: true }
+);
 
 const Vote = models?.Vote || model<IVote>("Vote", VoteSchema);
 
