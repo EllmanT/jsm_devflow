667f842ac56e460050954e361b8d89cf7df7ce90
EllmanT <tmuranda1@gmail.com>
2025-01-07 19:21:13 +0200

    Created the Tag, Tag-Question and the Vote Models

--- database/question.model.ts
+++ database/question.model.ts
@@ -11,18 +11,21 @@ export interface IQuestion {
   downvotes: number;
 }
 
-const QuestionSchema = new Schema<IQuestion>({
-  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
-  views: { type: Number, default: 0 },
-  answers: { type: Number, default: 0 },
-  upvotes: { type: Number, default: 0 },
-  downvotes: { type: Number, default: 0 },
-});
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
 
 const Question =
-  models?.question || model<IQuestion>("Question", QuestionSchema);
+  models?.Question || model<IQuestion>("Question", QuestionSchema);
 
 export default Question;
