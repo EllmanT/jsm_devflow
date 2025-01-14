da822e8515c9c16a59fbd66866578432bd8f93cd
EllmanT <tmuranda1@gmail.com>
2025-01-14 19:19:47 +0200

    Implemnted Sign  up

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 
 import AuthForm from "@/components/forms/AuthForm";
+import { signUpWithCredentials } from "@/lib/actions/auth";
 import { SignUpSchema } from "@/lib/validations";
 
 const SignUp = () => {
@@ -9,8 +10,8 @@ const SignUp = () => {
     <AuthForm
       formType="SIGN_UP"
       schema={SignUpSchema}
-      defaultValues={{ email: "", password: "" , name:"", username:""}}
-      onSubmit={(data) => Promise.resolve({ success: true })}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={signUpWithCredentials}
     />
   );
 };
