8fda3e9b8f69a4d0ba399d160947e54edb218cd4
EllmanT <tmuranda1@gmail.com>
2025-01-15 19:42:26 +0200

    Implement Create Question Form logic

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { MDXEditorMethods } from "@mdxeditor/editor";
+import { LoaderPinwheelIcon } from "lucide-react";
 import dynamic from "next/dynamic";
-import React, { useRef } from "react";
+import { useRouter } from "next/navigation";
+import React, { useRef, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import ROUTES from "@/constants/route";
+import { toast } from "@/hooks/use-toast";
+import { createQuestion } from "@/lib/actions/question.action";
 import { AskQuestionSchema } from "@/lib/validations";
 
 import TagCard from "../cards/TagCard";
@@ -26,7 +31,10 @@ const Editor = dynamic(() => import("@/components/editor"), {
 });
 
 const QuestionForm = () => {
+  const router = useRouter();
   const editorRef = useRef<MDXEditorMethods>(null);
+
+  const [isPending, startTransition] = useTransition();
   const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
@@ -73,7 +81,26 @@ const QuestionForm = () => {
       });
     }
   };
-  const handleCreateQuestion = () => {};
+  const handleCreateQuestion = async (
+    data: z.infer<typeof AskQuestionSchema>
+  ) => {
+    startTransition(async () => {
+      const result = await createQuestion(data);
+      if (!result.success) {
+        toast({
+          title: "Success",
+          description: "Question created successfully",
+        });
+        if (result.data) router.push(ROUTES.QUESTION(result.data._id));
+      } else {
+        toast({
+          title: `Error ${result.status}`,
+          description: result.error?.message || "Something went wrong",
+          variant: "destructive",
+        });
+      }
+    });
+  };
   return (
     <Form {...form}>
       <form
@@ -168,8 +195,18 @@ const QuestionForm = () => {
         <div className="mb-12 mt-8 flex justify-end">
           <Button
             type="submit"
+            disabled={isPending}
             className="primary-gradient w-fit !text-light-900"
           >
+            {isPending ? (
+              <>
+                <LoaderPinwheelIcon className="mr-2 size-4 animate-spin">
+                  Submitting
+                </LoaderPinwheelIcon>
+              </>
+            ) : (
+              <></>
+            )}
             Ask A Question
           </Button>
         </div>
